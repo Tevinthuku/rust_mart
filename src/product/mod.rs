48657c4598baa3b474a909bd649a3d051840f227
Tevin Thuku <tevinthuku@gmail.com>
2023-07-27 20:40:53 +0300

    product price update

--- src/product/mod.rs
+++ src/product/mod.rs
@@ -1,30 +1,12 @@
-use crate::{errors::ApiError, pool::DbPool, schema::product};
+mod model;
+
+use crate::{errors::ApiError, pool::DbPool};
 use actix_web::web;
 use anyhow::Context;
-use chrono::{DateTime, Utc};
-use diesel::prelude::*;
-use serde::Deserialize;
-use serde::Serialize;
 
-#[derive(Queryable, Selectable, Deserialize, Serialize)]
-#[diesel(table_name = product)]
-struct Product {
-    id: uuid::Uuid,
-    name: String,
-    description: String,
-    quantity_on_hand: i32,
-    quantity_available: i32,
-    price: Option<i32>,
-    created_at: DateTime<Utc>,
-}
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Insertable, Clone)]
-#[diesel(table_name = product)]
-struct ProductInput {
-    name: String,
-    description: String,
-    quantity_on_hand: i32,
-}
+use self::model::{Product, ProductInput};
 
 #[derive(Serialize)]
 struct ProductResponse {
@@ -73,10 +55,7 @@ async fn create_product(
 ) -> Result<web::Json<ProductResponse>, ApiError> {
     let response = web::block(move || -> Result<_, ApiError> {
         let mut conn = pool.get()?;
-        let product = data
-            .clone()
-            .insert_into(product::table)
-            .get_result::<Product>(&mut conn)?;
+        let product = Product::insert(data.clone(), &mut conn)?;
         ProductResponse::try_from(product)
     })
     .await??;
@@ -84,6 +63,34 @@ async fn create_product(
     Ok(web::Json(response))
 }
 
+#[derive(Deserialize)]
+struct PriceUpdate {
+    price: i32,
+}
+
+async fn update_price(
+    product_id: web::Path<uuid::Uuid>,
+    pool: web::Data<DbPool>,
+    data: web::Json<PriceUpdate>,
+) -> Result<web::Json<ProductResponse>, ApiError> {
+    let product = web::block(move || {
+        let mut conn = pool.get()?;
+        let updated_product =
+            Product::update_price(product_id.into_inner(), data.price, &mut conn)?;
+        ProductResponse::try_from(updated_product)
+    })
+    .await??;
+
+    Ok(web::Json(product))
+}
+
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::resource("/product").route(web::post().to(create_product)));
+    cfg.service(
+        web::scope("/product")
+            .service(web::resource("").route(web::post().to(create_product)))
+            .service(
+                web::scope("/{product_id}")
+                    .service(web::resource("").route(web::patch().to(update_price))),
+            ),
+    );
 }
---
+++ src/product/model.rs
@@ -0,0 +1,54 @@
+use crate::schema::product;
+
+use chrono::{DateTime, Utc};
+use diesel::prelude::*;
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Queryable, Selectable, AsChangeset, Identifiable, Deserialize, Serialize)]
+#[diesel(table_name = product)]
+pub struct Product {
+    pub id: uuid::Uuid,
+    pub name: String,
+    pub description: String,
+    pub quantity_on_hand: i32,
+    pub quantity_available: i32,
+    pub price: Option<i32>,
+    pub created_at: DateTime<Utc>,
+}
+
+#[derive(Deserialize, Insertable, Clone)]
+#[diesel(table_name = product)]
+pub struct ProductInput {
+    name: String,
+    description: String,
+    quantity_on_hand: i32,
+}
+
+impl Product {
+    pub fn insert(
+        input: ProductInput,
+        conn: &mut PgConnection,
+    ) -> Result<Product, diesel::result::Error> {
+        input
+            .insert_into(product::table)
+            .get_result::<Product>(conn)
+    }
+
+    pub fn update_price(
+        product_id: uuid::Uuid,
+        new_price: i32,
+        conn: &mut PgConnection,
+    ) -> Result<Product, diesel::result::Error> {
+        diesel::update(product::table.find(product_id))
+            .set(product::price.eq(Some(new_price)))
+            .get_result(conn)
+    }
+
+    pub fn find_by_id(
+        id: uuid::Uuid,
+        conn: &mut PgConnection,
+    ) -> Result<Product, diesel::result::Error> {
+        product::table.find(id).first(conn)
+    }
+}
